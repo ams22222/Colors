b287036783596a9c8ee13375622f0a18e2371382
Abel Millan <abel99ms@gmail.com>
2022-11-16 18:19:49 +0100

    colorbg

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -6,12 +6,7 @@
     <title>@yield('title')</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
   </head>
-  <body>
-    <style>
-        
-        background-color: linear-gradient(90deg, rgba(2,0,36,1) 0%, rgba(9,9,121,1) 35%, rgba(0,212,255,1) 100%);
-
-    </style>
+  <body style='background-color: #f9f9c5'>
     @yield('content')
   </body>
 </html>
